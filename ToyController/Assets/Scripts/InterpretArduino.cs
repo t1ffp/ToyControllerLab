62960cfcd5c0bfbe292b309b7e8558f8a9e31795
Tiff ☆彡 <pengt796@newschool.edu>
2024-03-01 09:23:20 -0500

    final commit

--- ToyController/Assets/Scripts/InterpretArduino.cs
+++ ToyController/Assets/Scripts/InterpretArduino.cs
@@ -8,9 +8,22 @@ public class InterpretArduino : MonoBehaviour
 
     public SerialController serialController;
 
+    public PetMeter petmeter;
+
+    public Animator bearHappy;
+
+    private void Start()
+    {
+        bearHappy = gameObject.GetComponent<Animator>();
+    }
+
+
     void OnMessageArrived(string msg)
     {
         Debug.Log(msg);
+
+        petmeter.currentHappiness += 5;
+        bearHappy.SetBool("isPlaying", true);
     }
 
     void OnConnectionEvent(bool success)
@@ -18,3 +31,4 @@ public class InterpretArduino : MonoBehaviour
         Debug.Log(success ? "Device connected" : "Device disconnected");
     }
 }
+
--- ToyController/Assets/Scripts/PetMeter.cs
+++ ToyController/Assets/Scripts/PetMeter.cs
@@ -2,18 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO.Ports;
 
 public class PetMeter : MonoBehaviour
 {
     public float startingHappiness = 100f;
-    public float decreaseRate = 1f;
+    public float decreaseRate = 20f;
     public float decreaseInterval = 1f;
     public float increaseAmount = 10f;
 
-    private float currentHappiness;
+    public float currentHappiness;
     private float timer;
 
     public Slider happinessSlider;
+
+    public SerialController serialcontroller;
 
     void Start()
     {
@@ -37,12 +40,25 @@ public class PetMeter : MonoBehaviour
         {
             IncreaseHappiness();
         }
+
+
     }
 
     void DecreaseHappiness()
     {
         currentHappiness -= decreaseRate;
         currentHappiness = Mathf.Max(0, currentHappiness);
+
+        //serialcontroller.SendSerialMessage(currentHappiness.ToString());
+
+        if(currentHappiness < 50)
+        {
+            serialcontroller.SendSerialMessage("A");
+        }
+        else if(currentHappiness > 50)
+        {
+            serialcontroller.SendSerialMessage("Z");
+        }
     }
 
     public void IncreaseHappiness()
